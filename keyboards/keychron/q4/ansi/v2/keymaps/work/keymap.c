a2fde46b5a31173b8fb6d0910ec80acfa9bed033
Малыгин Дмитрий Михайлович <malygin_dm@magnit.ru>
2025-09-25 21:35:41 +0300

    q4: remove unused enum

--- keyboards/keychron/q4/ansi/v2/keymaps/work/keymap.c
+++ keyboards/keychron/q4/ansi/v2/keymaps/work/keymap.c
@@ -27,10 +27,6 @@ enum layers {
     HOLD_CAPS
 };
 
-enum {
-    TD_LSFT_CAPS,
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_61(
         KC_ESC,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,         KC_BSPC,
