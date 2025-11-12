1b130774ceece77d1a0317b99b23d507af15c232
Малыгин Дмитрий Михайлович <malygin_dm@magnit.ru>
2025-11-12 21:27:48 +0300

    keychron: update mac layers

--- keyboards/keychron/q4/ansi/v2/keymaps/work/keymap.c
+++ keyboards/keychron/q4/ansi/v2/keymaps/work/keymap.c
@@ -29,7 +29,9 @@ enum layers {
 
 enum custom_keycodes {
     M_RSFT = SAFE_RANGE,
+    M_MAC_FN1,
     M_WIN_FN1,
+    M_FN1,
     M_FN2,
     M_RCTL,
     M_CAPS_LOCK,
@@ -37,14 +39,15 @@ enum custom_keycodes {
 
 static fast_timer_t caps_tap_timer = 0;
 static bool caps_pressed = false;
+static bool is_mac = false;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_61(
-        KC_ESC,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,         KC_BSPC,
+        KC_GRV,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,         KC_BSPC,
         KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,        KC_BSLS,
-        KC_CAPS, KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,                  KC_ENT,
-        KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,                  KC_RSFT,
-        KC_LCTL, KC_LOPT,  KC_LCMD,                            KC_SPC,                             KC_RCMD,  MO(MAC_FN1), MO(ALL_FN2), KC_RCTL),
+    M_CAPS_LOCK, KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,                  KC_ENT,
+        KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,                  M_RSFT,
+        KC_LCTL, KC_LOPT,  KC_LCMD,                            KC_SPC,                             KC_RCMD,  M_MAC_FN1, M_FN2, M_RCTL),
 
     [WIN_BASE] = LAYOUT_ansi_61(
         KC_GRV,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,         KC_BSPC,
@@ -54,10 +57,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,  KC_LGUI,  KC_LALT,                            KC_SPC,                             KC_RALT,  M_WIN_FN1, M_FN2,         M_RCTL),
 
     [MAC_FN1] = LAYOUT_ansi_61(
-        KC_GRV,  KC_BRID,  KC_BRIU, KC_NO,   KC_NO,   RM_VALD, RM_VALU, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  RM_NEXT,
-        RM_TOGG, RM_NEXT,  RM_VALU, RM_HUEU, RM_SATU, RM_SPDU, _______, _______, _______, _______, KC_INS,   KC_PGUP,  KC_HOME,  _______,
-        _______, RM_PREV,  RM_VALD, RM_HUED, RM_SATD, RM_SPDD, _______, _______, KC_UP,   KC_NO,   KC_PGDN,  KC_END,             _______,
-        _______,           _______, _______, _______, _______, _______, NK_TOGG, KC_LEFT, KC_DOWN, KC_RIGHT, KC_DEL,             _______,
+        _______,  KC_BRID,  KC_BRIU, KC_NO,   KC_NO,   RM_VALD, RM_VALU, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  RM_NEXT,
+        _______,  KC_HOME,  KC_UP,   KC_END, _______, _______, _______, _______, _______, _______, _______,   _______,  _______,  _______,
+        _______,  KC_LEFT,  KC_DOWN,KC_RIGHT, _______, _______, _______, _______, _______, _______, _______, _______,             _______,
+        _______,           _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,             _______,
         _______, _______,  _______,                            _______,                            _______,  _______,  _______,  _______),
 
     [WIN_FN1] = LAYOUT_ansi_61(
@@ -73,13 +76,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, RM_PREV,  RM_VALD, RM_HUED, RM_SATD, RM_SPDD, _______, _______, _______, _______, _______,  _______,                  _______,
         _______,           _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,                  _______,
         _______, _______,  _______,                            _______,                            _______,  _______,  _______,        _______),
-    
+
     [HOLD_CAPS] = LAYOUT_ansi_61(
         XXXXXXX, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,         KC_DEL,
         XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGUP, KC_PGDN, KC_END,   XXXXXXX,  XXXXXXX,        KC_CAPS,
         XXXXXXX, XXXXXXX,  XXXXXXX,  KC_DEL, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,XXXXXXX,  XXXXXXX,                  KC_INS,
         _______,           XXXXXXX, XXXXXXX, C(KC_C), C(KC_V), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,                  M_RSFT,
-        _______, _______,  _______,                            _______,                            XXXXXXX,  M_WIN_FN1, M_FN2,         M_RCTL)
+        _______, _______,  _______,                            _______,                            XXXXXXX,  M_FN1,    M_FN2,          M_RCTL)
                                                             /* ^^^^^^^caps word*/
 };
 
@@ -114,16 +117,48 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             unregister_code(KC_RSFT);
         }
         return false;
+    case M_MAC_FN1:
+        if (record->event.pressed) {
+            if (is_left_mode_active || caps_layer_active) {
+                register_code(KC_LEFT);
+            } else {
+                layer_on(MAC_FN1);
+            }
+        } else {
+            unregister_code(KC_LEFT);
+            layer_off(MAC_FN1);
+        }
+        return false;
     case M_WIN_FN1:
         if (record->event.pressed) {
             if (is_left_mode_active || caps_layer_active) {
                 register_code(KC_LEFT);
-            } else {    
+            } else {
                 layer_on(WIN_FN1);
             }
         } else {
             unregister_code(KC_LEFT);
             layer_off(WIN_FN1);
+        }
+        return false;
+    case M_FN1:
+        if (record->event.pressed) {
+            if (is_left_mode_active || caps_layer_active) {
+                register_code(KC_LEFT);
+            } else {
+                if (is_mac) {
+                    layer_on(MAC_FN1);
+                } else {
+                    layer_on(WIN_FN1);
+                }
+            }
+        } else {
+            unregister_code(KC_LEFT);
+            if (is_mac) {
+                layer_off(MAC_FN1);
+            } else {
+                layer_off(WIN_FN1);
+            }
         }
         return false;
     case M_FN2:
@@ -158,6 +193,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     default:
         return true;
     }
+}
+
+bool dip_switch_update_user(uint8_t index, bool active) {
+    switch (index) {
+        case 0:
+        is_mac = !active;
+        case 1:
+        is_mac = active;
+    }
+    return true;
 }
 
 // clang-format on
