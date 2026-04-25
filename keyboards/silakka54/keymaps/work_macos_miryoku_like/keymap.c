1d75501963a8ce6843506206db853e31199463b2
Малыгин Дмитрий Михайлович <malygin_dm@magnit.ru>
2026-04-26 00:15:50 +0300

    silakka54: update miryoku-like keymap

--- keyboards/silakka54/keymaps/work_macos_miryoku_like/keymap.c
+++ keyboards/silakka54/keymaps/work_macos_miryoku_like/keymap.c
@@ -14,7 +14,7 @@ enum layers {
     BASE,
     NAV,
     MEDIA,
-    // NUM,
+    NUM,
     // SYM,
     FUN,
 };
@@ -23,43 +23,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
         KC_GRV,  KC_1,         KC_2,         KC_3,         KC_4,         KC_5,                               KC_6,    KC_7,         KC_8,         KC_9,         KC_0,            KC_MINS,
         KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                               KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,            KC_EQL,
-        KC_ESC,  LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G,                               KC_H,    LSFT_T(KC_J), LGUI_T(KC_K), LALT_T(KC_L), LCTL_T(KC_SCLN), KC_QUOT,
-        KC_LBRC, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,                               KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_RBRC,
-                                                     LT(MEDIA, KC_BSLS), LT(NAV, KC_ESC), KC_SPC,    KC_ENT, KC_BSPC, LT(FUN, KC_DEL)
+        KC_ESC,  LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G,                               KC_H,    RSFT_T(KC_J), RGUI_T(KC_K), LALT_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT,
+LT(MEDIA,KC_LBRC), KC_Z,       KC_X,         KC_C,         KC_V,         KC_B,                               KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_RBRC,
+                                                     LT(NUM, KC_BSLS), MO(NAV), KC_SPC,              KC_ENT, KC_BSPC, LT(FUN, KC_DEL)
     ),                                                                                              //LT(SYM, KC_ENT), LT(NUM, KC_BSPC), LT(FUN, KC_DEL)
     [NAV] = LAYOUT(
         XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               U_RDO,    U_PST,   U_CPY,   U_CUT,   U_UND,   XXXXXXX,
-        U_LANG,   KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, XXXXXXX,                                               KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, CW_TOGG,
-        XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               KC_HOME,  KC_PGDN, KC_PGUP, KC_END,  KC_INS,  KC_CAPS,
-                                             XXXXXXX, XXXXXXX, XXXXXXX,                              KC_ENT, KC_BSPC,  KC_DEL
+        XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, U_RDO,   XXXXXXX, /*TODO: do I need undo and clipboard here?->*/U_UND,    U_PST,   U_CPY,   U_CUT,   KC_LBRC, KC_RBRC,
+        U_LANG,   KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, XXXXXXX,                                               KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT, CW_TOGG, KC_CAPS,
+        XXXXXXX,  U_UND,   U_CUT,   U_CPY,   U_PST,   XXXXXXX,                                               KC_HOME,  KC_PGDN, KC_PGUP, KC_END,  KC_BSLS, KC_INS,
+                                             XXXXXXX, _______, XXXXXXX,                              KC_ENT, KC_BSPC,  KC_DEL
     ),
     [MEDIA] = LAYOUT(
         XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX,  KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, XXXXXXX,                                               KC_MPRV,  KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX, XXXXXXX,
+        XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               KC_MPRV,  KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX, XXXXXXX,
         XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                              XXXXXXX, XXXXXXX, XXXXXXX,                             KC_MSTP, KC_MPLY,  KC_MUTE
     ),
-    // [NUM] = LAYOUT(
-    //     XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    //     XXXXXXX,  KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC,                                               XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    //     XXXXXXX,  KC_SCLN, KC_4,    KC_5,    KC_6,    KC_EQL,                                                XXXXXXX,  KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
-    //     XXXXXXX,  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_SLSH,                                               XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    //                                          KC_DOT,  KC_0,  KC_MINS,                               XXXXXXX, XXXXXXX,  XXXXXXX
-    // ),
+    [NUM] = LAYOUT(
+        XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               KC_LBRC,  KC_7,    KC_8,    KC_9,    KC_RBRC, XXXXXXX,
+        XXXXXXX,  KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, XXXXXXX,                                               KC_MINS,  KC_4,    KC_5,    KC_6,    KC_EQL,  XXXXXXX,
+        XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               KC_SCLN,  KC_1,    KC_2,    KC_3,    KC_SLSH, XXXXXXX,
+                                             XXXXXXX, XXXXXXX, XXXXXXX,                             KC_COMM, KC_0, KC_DOT
+    ),
     // [SYM] = LAYOUT(
     //     XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     //     XXXXXXX,  KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                                               XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    //     XXXXXXX,  KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,                                               XXXXXXX,  KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
+    //     XXXXXXX,  KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,                                               XXXXXXX,  KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI, XXXXXXX,
     //     XXXXXXX,  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE,                                               XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     //                                          KC_LPRN, KC_RPRN, KC_UNDS,                             XXXXXXX, XXXXXXX,  XXXXXXX
     // ),
     [FUN] = LAYOUT(
         XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX,  KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR,                                               XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX,  KC_F11,  KC_F4,   KC_F5,   KC_F6,   XXXXXXX,                                               XXXXXXX,  KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL, XXXXXXX,
+        XXXXXXX,  KC_F11,  KC_F4,   KC_F5,   KC_F6,   XXXXXXX,                                               XXXXXXX,  KC_RSFT, KC_RGUI, KC_LALT, KC_RCTL, XXXXXXX,
         XXXXXXX,  KC_F10,  KC_F1,   KC_F2,   KC_F3,   XXXXXXX,                                               XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                             XXXXXXX, KC_SPC,  KC_TAB,                              XXXXXXX, XXXXXXX,  XXXXXXX
+                                             XXXXXXX, XXXXXXX,  XXXXXXX,                            XXXXXXX, XXXXXXX,  XXXXXXX
     ),
 };
