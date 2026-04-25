058ba2508d5b1100b5913a0ea744c8782d06d21c
Малыгин Дмитрий Михайлович <malygin_dm@magnit.ru>
2026-04-25 21:54:38 +0300

    silakka54: timeless home row mods

--- keyboards/silakka54/keymaps/work_macos_miryoku_like/config.h
+++ keyboards/silakka54/keymaps/work_macos_miryoku_like/config.h
@@ -1,5 +1,6 @@
-#undef TAPPING_TERM
-#define TAPPING_TERM 200
-
-// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define QUICK_TAP_TERM 0
+// https://www.reddit.com/r/ErgoMechKeyboards/comments/1q1jo3c/urobs_zmk_timeless_home_row_mods_ported_to_native/
+#define TAPPING_TERM 250
+#define PERMISSIVE_HOLD
+#define FLOW_TAP_TERM 150
+#define CHORDAL_HOLD
+#define SPECULATIVE_HOLD
