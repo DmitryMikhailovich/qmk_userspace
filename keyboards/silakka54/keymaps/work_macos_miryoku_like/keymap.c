e90ba57b3d9815a0187ebb09b28148883c26674b
Малыгин Дмитрий Михайлович <malygin_dm@magnit.ru>
2026-05-31 17:53:50 +0300

    silakka54: use normal num keycodes on NUM layer

--- keyboards/silakka54/keymaps/work_macos_miryoku_like/keymap.c
+++ keyboards/silakka54/keymaps/work_macos_miryoku_like/keymap.c
@@ -90,10 +90,10 @@ LT(MEDIA, KC_ESC),LCTL_T(KC_A),LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G,  
     ),
     [NUMPAD] = LAYOUT(
         XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               KC_PAST,  KC_P7,   KC_P8,   KC_P9,   KC_PMNS, XXXXXXX,
-        XXXXXXX,  KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, XXXXXXX,                                               KC_PEQL,  KC_P4,   KC_P5,   KC_P6,   KC_PPLS, XXXXXXX,
-        XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               KC_PENT,  KC_P1,   KC_P2,   KC_P3,   KC_PSLS, XXXXXXX,
-                                             _______, XXXXXXX, XXXXXXX,                               KC_P0, KC_BSPC,  KC_PDOT
+        XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               KC_PAST,  KC_7,    KC_8,    KC_9,   KC_PMNS, XXXXXXX,
+        XXXXXXX,  KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, XXXXXXX,                                               KC_PEQL,  KC_4,    KC_5,    KC_6,   KC_PPLS, XXXXXXX,
+        XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               KC_PENT,  KC_1,    KC_2,    KC_3,   KC_PSLS, XXXXXXX,
+                                             _______, XXXXXXX, XXXXXXX,                                KC_0, KC_BSPC,  KC_PDOT
     ),
     [FUN] = LAYOUT(
         XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                               XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
